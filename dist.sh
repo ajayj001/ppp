90a5991d29d46ee3b040aae62af38099510f2908
Jan van der Lugt <janlugt@gmail.com>
2013-02-01 15:20:25 +0100

    dist.sh and build.xml version number increase

--- dist.sh
+++ dist.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 tar czf jlt230_JanvanderLugt_mpi_2.tar.gz mpi/asp/
 tar czf jlt230_JanvanderLugt_gpu_2.tar.gz --exclude=gpu/images/* gpu/
-
+cd java
+	ant dist
+	mv jlt230_Jan\ van\ der\ Lugt_2.zip ../jlt230_JanvanderLugt_java_2.zip
+cd ..
