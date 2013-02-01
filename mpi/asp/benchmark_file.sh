8051721722b5dfabc0550537a075f384362788b6
Jan van der Lugt <janlugt@gmail.com>
2013-02-01 17:01:52 +0100

    benchmark scripts and results

---
+++ mpi/asp/benchmark_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+results=results_file
+for file in ../sample_graphs/test1.gr.new ../sample_graphs/test2.gr.new ../sample_graphs/test3.gr.new ../sample_graphs/test4.gr.new; do
+	echo $file >> $results
+	echo seq >> $results
+	for i in {1..3}; do
+		./run_asp_seq -file $file 2>&1 | grep "ASP took" | cut -f8 -d" " >> $results
+	done
+	for i in 1 2 4 8 12 16; do
+		echo par $i >> $results
+		for j in {1..3}; do
+			./run_asp_par -file $file $i 2>&1 | grep "ASP took" | cut -f8 -d" " >> $results
+		done
+	done
+done
---
+++ mpi/asp/benchmark_random.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+results=results_random
+for size in 1000 2000 3000; do
+	echo random $size >> $results
+	echo seq >> $results
+	for i in {1..3}; do
+		./run_asp_seq -random $size 0 2>&1 | grep "ASP took" | awk '{print $3}' >> $results
+	done
+	for i in 1 2 4 8 12 16; do
+		echo par $i >> $results
+		for j in {1..3}; do
+			./run_asp_par -random $size 0 $i 2>&1 | grep "ASP took" | awk '{print $3}' >> $results
+		done
+	done
+done
