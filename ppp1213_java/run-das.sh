a676fbf8aaef6fe9b5a45348e6fc7f925382789d
Jan van der Lugt <janlugt@gmail.com>
2013-02-01 15:13:09 +0100

    renamed java dir

--- ppp1213_java/run-das.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-POOLSIZE=$1
-POOLNAME=pool-$(date +%Y%m%d%H%M%S)-$(date +%N)
-
-prun -v -1 -np $POOLSIZE bin/java-run -Dibis.pool.name=$POOLNAME -Dibis.pool.size=$POOLSIZE -Dibis.server.address=fs0:4321 rubiks.ipl.Rubiks --twists 12
-
-wait
--- ppp1213_java/run-sequential.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bin/java-run rubiks.sequential.Rubiks
