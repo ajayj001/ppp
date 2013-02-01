127e44d2fc36ab2bc836815906c00814acbf685a
Jan van der Lugt <janlugt@gmail.com>
2013-02-01 16:36:53 +0100

    benchmark script

--- mpi/asp/benchmark.sh
+++ mpi/asp/benchmark.sh
@@ -3,12 +3,12 @@ for file in ../sample_graphs/test1.gr.new ../sample_graphs/test2.gr.new ../sampl
 	echo $file >> results
 	echo seq >> results
 	for i in {1..3}; do
-		./run-asp-seq $file 2>&1 | grep "ASP took" | cut -f8 -d" " >> results
+		./run_asp_seq -file $file 2>&1 | grep "ASP took" | cut -f8 -d" " >> results
 	done
 	for i in 1 2 4 8 12 16; do
 		echo par $i >> results
 		for j in {1..3}; do
-			./run-asp-par -file $file $i 2>&1 | grep "ASP took" | cut -f8 -d" " >> results
+			./run_asp_par -file $file $i 2>&1 | grep "ASP took" | cut -f8 -d" " >> results
 		done
 	done
 done
